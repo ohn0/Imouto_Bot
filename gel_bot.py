e538cde51ca54907a5e0447658d0f3002c2d1986
unknown <silverskinx@gmail.com>
2019-03-17 16:20:02 -0400

    moved hardcoded token on ignored file

--- gel_bot.py
+++ gel_bot.py
@@ -15,8 +15,9 @@ from UserCollection import UserCollection
 from UserStatTracker import UserStatTracker
 from auditor import Auditor
 
-
-clientID = 'NTI2NTA0Mzc2ODUzMDY5ODQ1.D2ScuQ.6bn-tOxaK65db0e6Cyz0ecYwPMM'
+configFile = open('token.config', 'r')
+clientID = configFile.readline()
+configFile.close()
 bot = commands.Bot(command_prefix='$', case_insensitive=True)
 
 auditor = Auditor("audit.txt")
@@ -39,6 +40,7 @@ async def kona(ctx, *, arg):
     if response != None:
         auditor.audit(str(ctx.message.author), response["auditMessage"][0], response["auditMessage"][1])
         userStats.updateStats(str(ctx.message.author))
+        print(response["response"])
         await ctx.send(response["response"])
         if(response["sendTags"]):
             await ctx.send("These are the tags I found with that image: \n" + response["tags"])
@@ -85,10 +87,8 @@ async def sfw(ctx, *, arg):
 async def stats(ctx):
     currentStats = userStats.getStats()
     sortedStats = sorted(currentStats.items(), key=lambda x: x[1],reverse=True)
-    # print(sortedStats)
-    statMessage = "```\n-----------------------------------------------------------\nHere is a list of the top freaks in this channel who have made the most requests to this little loli!\n"
+    statMessage = "```\n-----------------------------------------------------------\nHere is a list of the top freaks in this channel who have made the most successful requests to this little loli!\n"
     for stat in sortedStats:
-        # statMessage += (str(stat[0]) + "\t\t\t\t\t\t" + str(stat[1]) + "\n")
         statMessage += ("{:<30} {:<30}\n".format(str(stat[0]), str(stat[1])))
 
     statMessage += "-----------------------------------------------------------```"
