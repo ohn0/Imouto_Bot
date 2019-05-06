23c58011fc4a51b6319d4b2508ad8d891678d5ad
ohn0 <silverskinx@gmail.com>
2019-05-06 19:35:09 -0400

    added a default author to avoid the stats fucking up

--- auditor.py
+++ auditor.py
@@ -15,13 +15,14 @@ class Auditor:
         self.auditLines = self.textFile.readlines()
         self.textFile.close()
 
-    def audit(self, author, file_url, tags, auditSource = "n/a"):
+    def audit(self, author="null", file_url, tags, auditSource = "n/a"):
         self.textFile = open(self.filename, 'a')
         self.textFile.write(author + ", " + tags + ", " + file_url + ", " +  str(datetime.datetime.now()) + ", " +  auditSource  + "\n")
+        self.closeFile()
+        ##the region below should be refactored
         self.previousAudit["author"] = author
         self.previousAudit["file_url"] = file_url
         self.previousAudit["tags"] = tags
-        self.closeFile()
 
     def getInitialAuditLog(self):
         return self.auditLines
