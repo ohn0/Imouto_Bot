9e26cb8104f57a809a0119165239046e7ef4679d
ohn0 <silverskinx@gmail.com>
2019-05-06 19:38:38 -0400

    whooops

--- auditor.py
+++ auditor.py
@@ -15,10 +15,11 @@ class Auditor:
         self.auditLines = self.textFile.readlines()
         self.textFile.close()
 
-    def audit(self, author="null", file_url, tags, auditSource = "n/a"):
+    def audit(self, author, file_url, tags, auditSource = "n/a"):
         self.textFile = open(self.filename, 'a')
         self.textFile.write(author + ", " + tags + ", " + file_url + ", " +  str(datetime.datetime.now()) + ", " +  auditSource  + "\n")
         self.closeFile()
+
         ##the region below should be refactored
         self.previousAudit["author"] = author
         self.previousAudit["file_url"] = file_url
